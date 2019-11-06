437835b1718c274c94803f74e24d21d6e15b0b68
duduhali <a88b88c88>
2019-11-06 19:39:10 +0800

    change

--- fly/test.py
+++ fly/test.py
@@ -1,23 +1,29 @@
 
-
 width = 8
 height = 8
-
-class Material(object):
+class Material(object):#材质
+    attribute = None # 气质，光质，水质，石质，土质，木质，肉质
     colour = (255,255,255)  # 颜色
     transparent = 0 #透明度，[0,1],越大越透明
     solidity = 0.5  # 硬度 [0,1]
-    attribute = None  # 阴阳(冷热、刚柔) 五行 属性
-    energy = None #能量
-    energy_output_rate = 1 #能量输出转换率，默认100%
-    energy_input_rate = 1  #能量输入转换率，默认100%
+    flexibility = 0.5 #柔性 [0,1]
+    smoothness = 0.5 #光滑度 [0,1]
+    energy = 0 #能量 [0,1]
+    corrosive = 0 #腐蚀性 [0,1]
+    toxicity = 0 #毒性 [0,1]
 
+    energy_output_rate = 1 #能量输出转换率，[0,1]
+    energy_input_rate = 1  #能量输入转换率，[0,1]
+    talent = []  # 禀赋
+
+class Stuff(object):#原料
+    sub = [] #附属：材质, 原料
+    position = [(0, 0)]  # 位置
+    # shape = []  # 形状: 圆形，三角形，四方形
 
 class Plant(object):
-    position = (0,0) #位置
-    shape = None #形状
-    material = Material() #材质
-    talent = None #天赋
+    material = Stuff() #材质
+    talent = [] #天赋
 
 
 
