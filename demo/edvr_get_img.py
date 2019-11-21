76d3c2b932a1944bd2fde35ecf2858f875b91b18
duduhali <a88b88c88>
2019-11-21 19:27:46 +0800

    添加EDVR

---
+++ demo/edvr_get_img.py
@@ -0,0 +1,36 @@
+#为EDVR模型准备数据
+
+import os
+import cv2
+
+
+def getImg(folder_file,sub_folder):
+    # 视频100帧
+    cap = cv2.VideoCapture(folder_file)
+    (flag,frame) = cap.read()
+    i = 0
+    while flag:
+        img_name = os.path.join(sub_folder,'%06d.jpg'%i)
+        frame = cv2.resize(frame, (640, 480))
+        cv2.imwrite(img_name, frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+        # 描述图片质量 范围0-100 有损压缩，为0 时压缩比最高
+        (flag, frame) = cap.read()
+        i += 1
+
+def edvr(f,f_tag):
+    for file in os.listdir(f):
+        file_name, extend = os.path.splitext(file)  # '10091373'  '.mp4'
+        folder_file = os.path.join(f,file)
+        sub_folder = os.path.join(f_tag,file_name)
+        if os.path.exists(sub_folder) == False:
+            os.makedirs(sub_folder)
+            print(sub_folder)
+
+        getImg(folder_file,sub_folder)
+
+gt = 'E:/videos/gt'
+gt_tag = 'D:/WorkPreject/AI4K/EDVR-Pytorch/datasets/gt'
+input = 'E:/videos/input'
+input_tag = 'D:/WorkPreject/AI4K/EDVR-Pytorch/datasets/input'
+edvr(input,input_tag)
+
