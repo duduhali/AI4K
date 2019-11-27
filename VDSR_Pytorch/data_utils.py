96c4885f084a7f6ecae8e855413c9fa701c131aa
duduhali <a88b88c88>
2019-11-27 10:39:48 +0800

    修改 VDSR

--- VDSR_Pytorch/data_utils.py
+++ VDSR_Pytorch/data_utils.py
@@ -1,16 +1,81 @@
-import torch.utils.data as data
+from PIL import Image
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import Compose, CenterCrop, Resize
 import torch
-import h5py
 
-class DatasetFromHdf5(data.Dataset):
-    def __init__(self, file_path):
-        super(DatasetFromHdf5, self).__init__()
-        hf = h5py.File(file_path)
-        self.data = hf.get('data')
-        self.target = hf.get('label')
+def calc_psnr(img1, img2):
+    return 10. * torch.log10(1. / torch.mean((img1 - img2) ** 2))
+
+
+def input_transform(crop_size,upscale_factor):
+    return Compose([
+        CenterCrop(crop_size),
+        # Resize(crop_size//upscale_factor, interpolation=Image.BICUBIC)
+    ])
+
+def target_transform(crop_size):
+    return Compose([
+        CenterCrop(crop_size)
+    ])
+
+class DatasetFromFolder(Dataset):
+    def __init__(self, image_filenames,target_filenames, input_transform=None, target_transform=None):
+        super(DatasetFromFolder, self).__init__()
+        # self.image_dir = dataset_dir + '/X4'
+        # self.target_dir = dataset_dir + '/gt'
+        # self.image_filenames = [join(self.image_dir, x) for x in listdir(self.image_dir) if is_image_file(x)]
+        # self.target_filenames = [join(self.target_dir, x) for x in listdir(self.target_dir) if is_image_file(x)]
+        self.image_filenames = image_filenames
+        self.target_filenames = target_filenames
+        self.input_transform = input_transform
+        self.target_transform = target_transform
 
     def __getitem__(self, index):
-        return torch.from_numpy(self.data[index,:,:,:]).float(), torch.from_numpy(self.target[index,:,:,:]).float()
-        
+        image, _, _ = Image.open(self.image_filenames[index]).convert('YCbCr').split()
+        target, _, _ = Image.open(self.target_filenames[index]).convert('YCbCr').split()
+        if self.input_transform:
+            image = self.input_transform(image)
+        if self.target_transform:
+            target = self.target_transform(target)
+
+        return image, target
+
     def __len__(self):
-        return self.data.shape[0]
+        return len(self.image_filenames)
+
+if __name__ == "__main__":
+    import torchvision.transforms as transforms
+    import glob,os
+    import numpy as np
+    from torch.utils.data import DataLoader
+
+    LR = 'E:/pngs_cut20/X4'  # (960, 540)
+    HR = 'E:/pngs_cut20/gt'  # (3840, 2160)  3840/960 = 2160/540 = 4
+    L_files = glob.glob(os.path.join(LR, "*"))
+    H_files = glob.glob(os.path.join(HR, "*"))
+    L_files = L_files[0:64]
+    H_files = H_files[0:64]
+
+    L_files = np.array(L_files)
+    H_files = np.array(H_files)
+
+    train_val_scale = 0.8
+    train_L = L_files[0:int(len(L_files) * train_val_scale)]
+    train_H = H_files[0:int(len(H_files) * train_val_scale)]
+    val_L = L_files[int(len(L_files) * train_val_scale):]
+    val_H = H_files[int(len(H_files) * train_val_scale):]
+
+
+
+    train_set = DatasetFromFolder(train_L, train_H, input_transform=Compose([Resize(( 216, 384), interpolation=Image.BICUBIC),transforms.ToTensor()]),
+                                  target_transform=transforms.ToTensor())
+    val_set = DatasetFromFolder(val_L, val_H, input_transform=Compose([Resize(( 216, 384), interpolation=Image.BICUBIC),transforms.ToTensor()]),
+                                target_transform=transforms.ToTensor())
+    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=16, shuffle=True)
+    val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=16, shuffle=False)
+
+    for data in train_loader:
+        inputs, labels = data
+        print(inputs.shape,labels.shape)
+        print(labels[0])
+        break
--- VDSR_Pytorch/train.py
+++ VDSR_Pytorch/train.py
@@ -1,90 +1,130 @@
-import argparse, os
+import os,glob
+import numpy as np
 import torch
 import random
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.optim as optim
-from torch.autograd import Variable
 from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchnet.meter as meter
+from tqdm import tqdm
+from PIL import Image
+from torchvision.transforms import Compose, Resize
 from VDSR_Pytorch.model import VDSR
-from VDSR_Pytorch.data_utils import DatasetFromHdf5
-
+from VDSR_Pytorch.data_utils import DatasetFromFolder,calc_psnr
 
 def adjust_learning_rate(epoch):
     return lr * (0.1 ** (epoch // step))
 
-def train(training_data_loader, optimizer, model, criterion, epoch):
-    lr = adjust_learning_rate(epoch-1)
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
-    print("Epoch = {}, lr = {}".format(epoch, optimizer.param_groups[0]["lr"]))
 
-    model.train()
-    for iteration, batch in enumerate(training_data_loader, 1):
-        input, target = Variable(batch[0]), Variable(batch[1], requires_grad=False)
-        if cuda:
-            input = input.cuda()
-            target = target.cuda()
+EPOCHS = 100
+BATCH_SIZE = 8
+num_workers = 0
 
-        loss = criterion(model(input), target)
-        optimizer.zero_grad()
-        loss.backward()
-        nn.utils.clip_grad_norm(model.parameters(),clip)
-        optimizer.step()
+momentum = 0.9
+weight_decay = 1e-4
+lr = 0.1
+step = 8
+start_epoch = 0
+clip = 0.4
+model_pth = ''
+# model_pth = 'checkpoint/model_epoch_9.pth'
+cuda = torch.cuda.is_available()
+if __name__ == "__main__":
+    # model = VDSR()
+    # print(model.parameters)
 
-        if iteration%100 == 0:
-            print("===> Epoch[{}]({}/{}): Loss: {:.10f}".format(epoch, iteration, len(training_data_loader), loss.data[0]))
+    #准备数据
+    LR = 'E:/pngs_cut20/X4'  # (960, 540)
+    HR = 'E:/pngs_cut20/gt'  # (3840, 2160)  3840/960 = 2160/540 = 4
+    L_files = glob.glob(os.path.join(LR, "*"))
+    H_files = glob.glob(os.path.join(HR, "*"))
+    # L_files = L_files[0:100]   #####################
+    # H_files = H_files[0:100]   #####################
 
-def save_checkpoint(model, epoch):
-    model_out_path = "checkpoint/" + "model_epoch_{}.pth".format(epoch)
-    if not os.path.exists("checkpoint/"):
-        os.makedirs("checkpoint/")
-    torch.save({'state_dict': model.state_dict(),"epoch": epoch}, model_out_path)
-    print("Checkpoint saved to {}".format(model_out_path))
+    L_files = np.array(L_files)
+    H_files = np.array(H_files)
+    train_val_scale = 0.9
+    train_L = L_files[0:int(len(L_files) * train_val_scale)]
+    train_H = H_files[0:int(len(H_files) * train_val_scale)]
+    val_L = L_files[int(len(L_files) * train_val_scale):]
+    val_H = H_files[int(len(H_files) * train_val_scale):]
 
+    train_set = DatasetFromFolder(train_L, train_H, input_transform=Compose(
+        [Resize((216, 384), interpolation=Image.BICUBIC), transforms.ToTensor()]),
+                                  target_transform=transforms.ToTensor())
+    val_set = DatasetFromFolder(val_L, val_H, input_transform=Compose(
+        [Resize((216, 384), interpolation=Image.BICUBIC), transforms.ToTensor()]),
+                                target_transform=transforms.ToTensor())
+    train_loader = DataLoader(dataset=train_set, num_workers=num_workers, batch_size=BATCH_SIZE, drop_last=True,shuffle=True)
+    val_loader = DataLoader(dataset=val_set, num_workers=num_workers, batch_size=BATCH_SIZE, drop_last=True,shuffle=False)
 
-def main():
+    #准备模型
     seed = random.randint(1, 10000)
     torch.manual_seed(seed)
     if cuda:
         torch.cuda.manual_seed(seed)
     cudnn.benchmark = True
 
-    # train_set = DatasetFromHdf5("data/train.h5")
-    # training_data_loader = DataLoader(dataset=train_set, num_workers=num_workers, batch_size=batchSize, shuffle=True)
-
     model = VDSR()
-    criterion = nn.MSELoss(size_average=False)  # 返回向量形式
-    optimizer = optim.SGD(model.parameters(), lr, momentum=momentum, weight_decay=weight_decay)
+    criterion = nn.MSELoss()
     if cuda:
         model = model.cuda()
         criterion = criterion.cuda()
+    print('# parameters:', sum(param.numel() for param in model.parameters()))
+    optimizer = optim.SGD(model.parameters(), lr, momentum=momentum, weight_decay=weight_decay)
 
     if os.path.isfile(model_pth):
         checkpoint = torch.load(model_pth)
         start_epoch = checkpoint["epoch"] + 1
-        model.load_state_dict(checkpoint["state_dict"].state_dict())
+        model.load_state_dict(checkpoint["state_dict"])
 
-    print("===> Training")
-    for epoch in range(start_epoch, nEpochs + 1):
-        train(training_data_loader, optimizer, model, criterion, epoch)
-        save_checkpoint(model, epoch)
+    #开始训练
+    for epoch in range(start_epoch,EPOCHS):
+        model.train()
 
-nEpochs = 50
-batchSize = 128
-num_workers = 2
-momentum = 0.9
-weight_decay = 1e-4
-lr = 0.1
-step = 10
-start_epoch = 1
-clip = 0.4
-model_pth = ''
-cuda = torch.cuda.is_available()
-if __name__ == "__main__":
-    # model = VDSR()
-    # print(model.parameters)
+        lr = adjust_learning_rate(epoch)
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
 
-    main()
+        epoch_losses = meter.AverageValueMeter()
+        with tqdm(total=(len(train_set) - len(train_set) % BATCH_SIZE)) as t:
+            t.set_description('epoch:{}/{}, lr={}'.format(epoch, EPOCHS - 1,optimizer.param_groups[0]["lr"]))
+            for data in train_loader:
+                inputs, labels = data
+                if cuda:
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
 
+                preds = model(inputs)
+                loss = criterion(preds, labels)
+
+                epoch_losses.add(loss.item(), len(inputs))
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), clip)
+                optimizer.step()
+
+                t.set_postfix(loss='{:.8f}'.format(epoch_losses.value()[0]))
+                t.update(len(inputs))
+
+        model.eval()
+        epoch_psnr = meter.AverageValueMeter()
+
+        for data in val_loader:
+            inputs, labels = data
+            if cuda:
+                inputs = inputs.cuda()
+                labels = labels.cuda()
+
+            with torch.no_grad():
+                preds = model(inputs).clamp(0.0, 1.0)
+            epoch_psnr.add(calc_psnr(preds, labels), len(inputs))
+
+        print('eval psnr: {:.2f}'.format(epoch_psnr.value()[0]))
+
+        # if (epoch+1) % 5 == 0:
+        torch.save({'state_dict': model.state_dict(), "epoch": epoch},
+                       "checkpoint/epoch_{}_psnr_{:.2f}.pth".format(epoch,epoch_psnr.value()[0]))
 
