eeb8d48a3943c3a08a163387bb56c138ef34b22d
duduhali <a88b88c88>
2019-12-17 02:37:19 +0800

    完善模型

--- MYSR/test.py
+++ MYSR/test.py
@@ -1,31 +1,34 @@
 import torch
-import argparse
-from model.SR import SR
+import torch.nn as nn
+from torch.autograd import Variable
 
+#输入维度(seq,batch,feature)
+#记忆维度(num_layers*num_directions,batch,hidden)
+#num_layers表示RNN单元堆叠的层数，num_directions为1(单向RNN)和2（双向RNN）
 
-parser = argparse.ArgumentParser()
-
-
-args = parser.parse_args()
-
-args.n_resgroups = 1
-args.n_res_blocks = 2
-
-args.scale = 4
-args.n_feats = 64
-args.reduction = 16
-args.rgb_range = 255
-args.n_colors = 3
-args.res_scale = 0.1 #模型中未使用
-
-
-model = SR(args)
-print(model)
+#output表示每个时刻网络最后一层的输出，维度是(seq,batch,hidden*num_directions)
+# hn是最后时刻所有堆叠的记忆单元的所有输出，维度为(num_layers*num_directions, batch,hidden)
+rnn = nn.RNN(2, 4, 1)
+# nn.RNN(input_size=20, hidden_size=50, num_layers=2)
+input = torch.randn(3, 1, 2)
+print(input)
+h0 = torch.randn(1, 1, 4)
+print(h0)
+output, hn = rnn(input, h0)
+print(output)
+print(output.shape, hn.shape)
 
 
 
 
-
-
-
-
+# rnn = nn.LSTM(10, 20, 2)
+# input = torch.randn(5, 3, 10)
+# h0 = torch.randn(2, 3, 20)
+# c0 = torch.randn(2, 3, 20)
+# output, (hn, cn) = rnn(input, (h0, c0))
+#
+#
+# rnn = nn.GRU(10, 20, 2)
+# input = torch.randn(5, 3, 10)
+# h0 = torch.randn(2, 3, 20)
+# output, hn = rnn(input, h0)
